d16dee5b258a60c93585de7b0e9d910b240162d8
Killerulya2015 <Killerulya2015@yandex.ru>
2025-05-15 11:15:24 +0000

    done

--- main.py
+++ main.py
@@ -9,10 +9,25 @@ def index():
     with open('index.html', 'r') as f:
         return make_response(f.read(), 200, {'Content-type': 'text/html'})
 
-@app.route("/contact", methods=['GET'])
+@app.route("/contact.html", methods=['GET'])
 def contact():
- with open('contact.html', 'r') as f:
- return make_response(f.read(), 200, {'Content-type': 'text/html'})
+    with open('contact.html', 'r') as f:
+        return make_response(f.read(), 200, {'Content-type': 'text/html'})
+
+@app.route("/products.html", methods=['GET'])
+def products():
+    with open('products.html', 'r') as f:
+        return make_response(f.read(), 200, {'Content-type': 'text/html'})
+
+@app.route("/category.html", methods=['GET'])
+def category():
+    with open('category.html', 'r') as f:
+        return make_response(f.read(), 200, {'Content-type': 'text/html'})
+
+@app.route("/orders", methods=['GET'])
+def orders():
+    with open('category.html', 'r') as f:
+        return make_response(f.read(), 200, {'Content-type': 'text/html'})
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
